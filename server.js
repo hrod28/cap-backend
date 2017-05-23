123c89065673613cfe58c46d91504ae5b9090c65
Heidi Rodriguez <hrod2645@gmail.com>
2017-05-23 07:47:12 -0600

    add file to server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const brewers = require('./routes/brewers');
 const votes = require('./routes/votes');
 const comments = require('./routes/comments');
 // const visited_chefs = require('./routes/visited_chefs');
+const thanks = require('./routes/thanks');
 const dishes = require('./routes/dishes');
 
 app.use('/api/users', users);
@@ -25,6 +26,7 @@ app.use('/api/chefs', chefs);
 app.use('/api/brewers', brewers);
 app.use('/api/posts', posts);
 app.use('/api/votes', votes);
+app.use('/api/thanks', thanks);
 app.use('/api/comments', comments);
 // app.use('/api/visited_chefs', visited_chefs);
 app.use('/api/dishes', dishes);
